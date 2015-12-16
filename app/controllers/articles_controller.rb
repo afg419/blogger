92149aa3fdbde280e38503ebdc6879585fff898d
Aaron Greenspan <afg419@gmail.com>
2015-12-16 15:13:23 -0700

    forms completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 
   def index
     @articles = Article.all
@@ -9,7 +10,37 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
+
+  def create
+    a= Article.create(create_article_params)
+    flash.notice = "Article with title '#{a.title}' created!"
+    redirect_to articles_path
+  end
+
+  def destroy
+    flash.notice = "Article '#{Article.find(params[:id]).title}' destroyed!"
+    Article.find(params[:id]).destroy
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(create_article_params)
+
+    flash.notice = "Article '#{@article.title}' updated!"
+
+    redirect_to article_path(@article)
+  end
+
+
+private
+
 
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,5 @@
 module ArticlesHelper
+  def create_article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
